afbdd8fb377ab285c77d2311161dfff5df75ebd9
Josh RosenHanst <josh.rosenhanst@gmail.com>
2019-04-11 14:24:57 -0400

    add sample project files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <main id="site-main">
+      <h1>Hello World</h1>
+      <img src="assets/img/vue_logo.png" alt="Vue logo">
+    </main>
+    <dog-footer />
+  </div>
+</template>
+
+<script>
+import DogFooter from './DogFooter.vue';
+export default {
+  name: 'App',
+  components: {
+    DogFooter
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  #site-main>img
+    width: 100px
+</style>
+
+
---
+++ src/DogFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <img id="dog_footer" :src="dog" alt="Transparent Dog footer">
+  </footer>
+</template>
+
+<script>
+import dog from './assets/img/dog.png';
+export default {
+  data: function(){
+    return {
+      dog: dog
+    };
+  }
+}
+</script>
+
